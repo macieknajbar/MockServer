88dbac79b2a2fb15191cc768c7eb9b956069244c
Maciej Najbar <maciek.najbar@gmail.com>
2017-05-10 09:56:32 -0400

    Service declaration and repository DTO.

---
+++ app/src/main/java/com/example/mockserver/rest/api/GitHubService.kt
@@ -0,0 +1,22 @@
+package com.example.mockserver.rest.api
+
+import com.example.mockserver.rest.dto.Repo
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * Server communication requests interface to GitHub's user's information.
+ *
+ * © This example is from Retrofit's official website adapted for Kotlin.
+ */
+interface GitHubService {
+
+    /**
+     * Request user's repositories.
+     * @param   user Username in GitHub
+     * @return List of specified users repositories.
+     */
+    @GET("users/{user}/repos")
+    fun listRepos(@Path("user") user: String): Call<List<Repo>>
+}
---
+++ app/src/main/java/com/example/mockserver/rest/dto/Repo.kt
@@ -0,0 +1,6 @@
+package com.example.mockserver.rest.dto
+
+/**
+ * Repository Data Transfer Object.
+ */
+data class Repo(val id: Long, val name: String, val stargazers_count: Int)
