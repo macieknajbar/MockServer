2e8ebf699cbd4597c7a173e20dad7d894c71d7b3
Maciej Najbar <maciek.najbar@gmail.com>
2017-05-14 10:03:30 +0200

    Rewrite responses for multiple requests and parsing responses to objects.

--- app/src/test/java/com/example/mockserver/rest/server/MockServerTest.kt
+++ app/src/test/java/com/example/mockserver/rest/server/MockServerTest.kt
@@ -4,6 +4,8 @@ import com.example.mockserver.rest.api.GitHubService
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import retrofit2.Call
+import retrofit2.http.GET
 
 class MockServerTest {
 
@@ -28,8 +30,42 @@ class MockServerTest {
 
     @Test fun `overrides response for the same request`() {
         server?.rewriteResponse("users/!!!/repos", 404, "null")
-        server?.rewriteResponse("users/!!!/repos", 500, "{message: \"internal error\"}")
+        server?.rewriteResponse("users/!!!/repos", 500, "{\"message\": \"internal error\"}")
         val response = server?.getService(GitHubService::class.java)?.listRepos("!!!")?.execute()
         Assert.assertEquals(500, response?.code())
     }
+
+    @Test fun `can rewrite responses for multiple requests and parse responses to objects`() {
+        server?.rewriteResponse("something/", 404, "null")
+        server?.rewriteResponse("users/!!!/repos", 200, reposFakeResponse)
+
+        val something = server?.getService(SomeAPI::class.java)?.something()?.execute()!!
+        val repos = server?.getService(GitHubService::class.java)?.listRepos("!!!")?.execute()!!
+
+        Assert.assertEquals(404, something.code())
+        Assert.assertEquals(200, repos.code())
+        Assert.assertEquals(2, repos.body().size)
+        Assert.assertEquals(2, repos.body()[1].id)
+        Assert.assertEquals("Mock Server", repos.body()[1].name)
+        Assert.assertEquals(302, repos.body()[1].stargazers_count)
+    }
+
+    interface SomeAPI {
+        @GET("something/")
+        fun something(): Call<Any>
+    }
+
+    var reposFakeResponse = "" +
+            "[" +
+            "  {" +
+            "    \"id\": 1," +
+            "    \"name\": \"Mock#1\"," +
+            "    \"stargazers_count\": 3" +
+            "  }," +
+            "  {" +
+            "    \"id\": 2," +
+            "    \"name\": \"Mock Server\"," +
+            "    \"stargazers_count\": 302" +
+            "  }" +
+            "]"
 }
